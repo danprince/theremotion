2341200200d40ec093c430967f04db0f2eac7070
danprince <danielprince@hotmail.co.uk>
2015-09-11 12:44:06 +0100

    rewriting hands as channels

--- src/theremotion/leap.cljs
+++ src/theremotion/leap.cljs
@@ -1,4 +1,33 @@
 (ns theremotion.leap
-  (:require [leapjs :as leapjs])
+  (:require [cljs.core.async :refer all]
+            [leapjs :as leapjs]))
 
+(def options {})
+
+(def listen (partial leapjs/loop options))
+
+(def frame-chan (chan))
+
+(def frame-mult (mult frame-chan))
+
+(defn find-hand [id hands]
+  (first
+    (filter
+      (fn [hand] (= id (.-id hand)))
+      hands)))
+
+(defn hand->chan [hand]
+  (let [id (.-id hand)
+        chan (chan)
+        fc (chan frame-mult)]
+    (while true
+      (let [frame (<!! fc)
+            hands (.-hands frame)
+            hand (find-hand id hands)]
+        (put! chan hand)))
+    (chan)))
+
+(listen
+  (fn [frame]
+    (put! frame-chan frame)))
 
